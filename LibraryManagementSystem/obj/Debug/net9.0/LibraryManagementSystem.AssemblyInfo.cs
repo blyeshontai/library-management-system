399cf23b7cfa3a2669041565c3f31a0b6da51ce3
Shontai Blye <blyeshontai@gmail.com>
2025-08-15 20:16:15 -0400

    Add LibraryManagementSystem and update README

--- LibraryManagementSystem/obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
+++ LibraryManagementSystem/obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b69b0fb040ddf407af7c71623cc1d82ce9dcf57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6398a5b654f16b9401f6e58e7c194e03bf3e6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
