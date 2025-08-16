7d2b41aa0f5ff403b44937b7adfc246c70f58c9b
Shontai Blye <blyeshontai@gmail.com>
2025-08-16 05:50:45 -0400

    Added step 5 and updated README

--- LibraryManagementSystem/obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
+++ LibraryManagementSystem/obj/Debug/net9.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6398a5b654f16b9401f6e58e7c194e03bf3e6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61eb499a33337ff6eef5f06c6ff187c50066adc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
